3077020bb53b075a1d5e87b6bb6dc0905cf20ecd
yuu-ux <yuugaehara@gmail.com>
2025-04-23 19:30:30 +0900

    :wrench: db の疎通確認

--- app/main.go
+++ app/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+    database "road_to_mixi/db"
 	"net/http"
-
 	"github.com/labstack/echo/v4"
+    "github.com/joho/godotenv"
+    "log"
 )
 
 func main() {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal(err)
+    }
+    db, err := database.New()
+    if err != nil {
+        log.Fatal(err)
+    }
+	defer db.Close()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
