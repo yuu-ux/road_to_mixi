534b1f1270659a36aecf3faff44f9c381142fb90
yuu-ux <yuugaehara@gmail.com>
2025-04-25 16:41:43 +0900

    :wrench: 機能追加
    - フレンドリスト取得
    - フレンドのフレンドリストを取得

--- app/main.go
+++ app/main.go
@@ -7,6 +7,7 @@ import (
     "github.com/joho/godotenv"
     "log"
     "road_to_mixi/handlers"
+    "road_to_mixi/models"
 )
 
 func main() {
@@ -18,11 +19,50 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-	defer db.Close()
 	e := echo.New()
     handlers.SetDefault(e)
+    database.InitDatabase(db)
+
+    var results []struct {
+        ID   int    `json:"id"`
+        Name string `json:"name"`
+    }
+
 	e.GET("/", func(c echo.Context) error {
-        return c.Render(http.StatusOK, "index.html", map[string]interface{}{"Title": "トップページ",})
+        var users []models.User
+        db.Find(&users)
+        return c.Render(http.StatusOK, "index.html", map[string]interface{}{"Title": "トップページ", "Users": users})
 	})
+
+    e.GET("/get_friend_list", func(c echo.Context) error {
+        param := c.QueryParam("id")
+        if err := db.Model(&models.FriendLink{}).
+            Select("User2.user_id AS id, User2.name").
+            Joins("User2").
+            Where("friend_links.user1_id = ?", param).
+            Scan(&results).Error; err != nil {
+            return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed"})
+        }
+        if len(results) == 0 {
+            return c.JSON(http.StatusNotFound, map[string]string{"error": "not found"})
+        }
+        return c.JSON(http.StatusOK, results)
+    })
+
+    e.GET("get_friend_of_friend_list", func(c echo.Context) error {
+        param := c.QueryParam("id")
+        subQuery := db.Model(&models.FriendLink{}).
+            Select("user2_id").
+            Where("user1_id = ?", param)
+        if err := db.Model(&models.FriendLink{}).
+            Select("User2.user_id AS id, User2.name AS name").
+            Joins("User2").
+            Where("friend_links.user1_id IN (?)", subQuery).
+            Scan(&results).Error; err != nil {
+            return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed"})
+            }
+        return c.JSON(http.StatusOK, results)
+    })
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
