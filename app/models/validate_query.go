3168f9f6267b0d0022da6091c9137e5c6bc5c2fb
yuu-ux <yuugaehara@gmail.com>
2025-05-07 00:58:27 +0900

    :wrench: バリデーション処理追加

--- app/models/validate_query.go
+++ app/models/validate_query.go
@@ -1,11 +1,11 @@
 package models
 
 type UserIDQuery struct {
-	ID int `query:"id" form:"id" validate:"required"`
+	ID int `query:"id" form:"id" validate:"required,min=1"`
 }
 
 type UserPagingQuery struct {
-	ID    int `query:"id" validate:"required"`
-	Page  int `query:"page" validate:"required"`
-	Limit int `query:"limit" validate:"required"`
+	ID    int `query:"id" validate:"required,min=1"`
+	Page  int `query:"page" validate:"required,min=1"`
+	Limit int `query:"limit" validate:"required,min=1,max=100"`
 }
