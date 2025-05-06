1736d896a59753625c7700741fb7a7b20b74886c
yuu-ux <yuugaehara@gmail.com>
2025-05-07 01:20:24 +0900

    :wrench: index を貼る

--- app/models/block_list.go
+++ app/models/block_list.go
@@ -2,8 +2,8 @@ package models
 
 type BlockList struct {
 	ID      int64 `json:"id"`
-	User1ID int   `json:"user1_id"`
-	User2ID int   `json:"user2_id"`
+	User1ID int   `json:"user1_id" gorm:"index"`
+	User2ID int   `json:"user2_id" gorm:"index"`
 	User1   User  `gorm:"foreignKey:User1ID;references:UserID"`
 	User2   User  `gorm:"foreignKey:User2ID;references:UserID"`
 }
--- app/models/users.go
+++ app/models/users.go
@@ -2,6 +2,6 @@ package models
 
 type User struct {
 	ID     int64  `json:"id"`
-	UserID int    `json:"user_id" gorm:"unique"`
+	UserID int    `json:"user_id" gorm:"index;unique"`
 	Name   string `json:"name"`
 }
